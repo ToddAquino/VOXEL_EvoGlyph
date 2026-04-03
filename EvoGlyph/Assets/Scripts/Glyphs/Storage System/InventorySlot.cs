5081dddd5b3df76f16e912afa0f90d474a5b132b
Todd Aquino <>
2026-04-03 18:55:18 +0800

    Removed glyph sequencer slot item draggin
    - Can no longer remove or switch glyphs in glyphsequencer by drag and drop
    - Will be replaced by cancel cast button to return to action menu

--- EvoGlyph/Assets/Scripts/Glyphs/Storage System/InventorySlot.cs
+++ EvoGlyph/Assets/Scripts/Glyphs/Storage System/InventorySlot.cs
@@ -2,40 +2,40 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventorySlot : MonoBehaviour,IBeginDragHandler,IDragHandler, IEndDragHandler, IDropHandler
+public class InventorySlot : MonoBehaviour/*,IBeginDragHandler,IDragHandler, IEndDragHandler, IDropHandler*/
 {
     public Glyph Item;
     public Image Icon;
     public bool IsEmpty => Item == null;
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("IsDragging");
-        InventoryDragHandler.Instance.BeginDrag(this);
-    }
-    public void OnDrag(PointerEventData eventData)
-    {
-        InventoryDragHandler.Instance.Drag(eventData.position);
-    }
+    //public void OnBeginDrag(PointerEventData eventData)
+    //{
+    //    Debug.Log("IsDragging");
+    //    InventoryDragHandler.Instance.BeginDrag(this);
+    //}
+    //public void OnDrag(PointerEventData eventData)
+    //{
+    //    InventoryDragHandler.Instance.Drag(eventData.position);
+    //}
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("IsDragging Ended");
-        if(eventData.pointerEnter == null || eventData.pointerEnter.GetComponent<InventorySlot>() == null)
-        {
-            InventorySlot slotToClear = InventoryDragHandler.Instance.originSlot;
-            InventoryContainer container = slotToClear.GetComponentInParent<InventoryContainer>();
-            if (container != null)
-            {
-                container.RemoveItemFromSlot(slotToClear);
-            }
-        }
-        InventoryDragHandler.Instance.EndDrag();
-    }
-    public void OnDrop(PointerEventData eventData)
-    {
-        InventoryDragHandler.Instance.HandleDrop(this);
-    }
+    //public void OnEndDrag(PointerEventData eventData)
+    //{
+    //    Debug.Log("IsDragging Ended");
+    //    if(eventData.pointerEnter == null || eventData.pointerEnter.GetComponent<InventorySlot>() == null)
+    //    {
+    //        InventorySlot slotToClear = InventoryDragHandler.Instance.originSlot;
+    //        InventoryContainer container = slotToClear.GetComponentInParent<InventoryContainer>();
+    //        if (container != null)
+    //        {
+    //            container.RemoveItemFromSlot(slotToClear);
+    //        }
+    //    }
+    //    InventoryDragHandler.Instance.EndDrag();
+    //}
+    //public void OnDrop(PointerEventData eventData)
+    //{
+    //    InventoryDragHandler.Instance.HandleDrop(this);
+    //}
 
     public void Set(Glyph item)
     {
