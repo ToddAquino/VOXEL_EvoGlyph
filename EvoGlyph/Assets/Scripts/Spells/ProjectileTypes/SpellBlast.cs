bf413b26a605464684993a0f54c774e59e8c3de1
Todd Aquino <>
2026-04-05 23:25:20 +0800

    Added x button - fix water blast projectile + intro cutscene (no subs)

--- EvoGlyph/Assets/Scripts/Spells/ProjectileTypes/SpellBlast.cs
+++ EvoGlyph/Assets/Scripts/Spells/ProjectileTypes/SpellBlast.cs
@@ -8,22 +8,62 @@ public class SpellBlast : Spell
     Vector3 startPosition;
     Vector3 targetPosition;
 
-    Vector3 hiddenProjectile;
+    //Vector3 hiddenProjectile;
     bool blastActive = false;
+
+    [Header("Beam")]
+    [SerializeField] LineRenderer lineRenderer;
+
 
     public override void Initialize(Unit target)
     {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (target.transform.position.x < this.transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
         spellTarget = target.gameObject;
 
-        hiddenProjectile = transform.position;
+        //hiddenProjectile = transform.position;
+        lineRenderer.positionCount = 2;
+        lineRenderer.enabled = true;
+
         startPosition = transform.position;
         targetPosition = spellTarget.transform.position;
 
+        lineRenderer.SetPosition(0, startPosition);
+        lineRenderer.SetPosition(1, startPosition);
         blastActive = true;
-        StartCoroutine(RunBlastProjectile());
+        //StartCoroutine(RunBlastProjectile());
+        StartCoroutine(ExtendBeamRoutine());
     }
 
-    private IEnumerator RunBlastProjectile()
+    //private IEnumerator RunBlastProjectile()
+    //{
+    //    float elapsed = 0f;
+
+    //    while (elapsed < extendDuration)
+    //    {
+    //        elapsed += Time.deltaTime;
+    //        float t = elapsed / extendDuration;
+
+    //        Vector3 currentPos = Vector3.Lerp(startPosition, targetPosition, t);
+    //        Vector3 hiddenProjectile = currentPos;
+
+    //        yield return null;
+    //    }
+    //    hiddenProjectile = targetPosition;
+    //    TriggerHit(spellTarget);
+    //    yield return new WaitForSeconds(0.4f);
+    //    blastActive = false;
+    //}
+
+    private IEnumerator ExtendBeamRoutine()
     {
         float elapsed = 0f;
 
@@ -32,12 +72,12 @@ public class SpellBlast : Spell
             elapsed += Time.deltaTime;
             float t = elapsed / extendDuration;
 
-            Vector3 currentPos = Vector3.Lerp(startPosition, targetPosition, t);
-            Vector3 hiddenProjectile = currentPos;
+            Vector3 currentEnd = Vector3.Lerp(startPosition, targetPosition, t);
+            lineRenderer.SetPosition(1, currentEnd);
 
             yield return null;
         }
-        hiddenProjectile = targetPosition;
+        lineRenderer.SetPosition(1, targetPosition);
         TriggerHit(spellTarget);
         yield return new WaitForSeconds(0.4f);
         blastActive = false;
